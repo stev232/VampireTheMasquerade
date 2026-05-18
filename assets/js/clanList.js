120ed30e42b6906941e997dd2454ea6c47b82f8c
Stephen Merki <stephen.merki232@gmail.com>
2026-05-18 14:58:59 -0500

    Updated the clan list to include the disciplines available to each clan.

--- assets/js/clanList.js
+++ assets/js/clanList.js
@@ -1,58 +1,120 @@
 const clanList = {
-    fifthEdition: [
+    "fifthEdition": [
         { 
-            clanName: "Banu Haqim" 
-        },
-        {
-            clanName: "Brujah",
-            clanDisciplines: [{ discipline: "Celerity" }, { discipline: "Potence" }, { discipline: "Presence" }],
-            archetypeList: [
-                { archetype: "Cancer In The System" },
-                { archetype: "Voice Of The People" },
-                { archetype: "Blood Worshipper" },
-                { archetype: "Trolling Punk" },
-                { archetype: "Monster In Disguise" }
+            "clanName": "Banu Haqim",
+            "clanDisciplines": [{ "discipline": "Blood Sorcery" }, { "discipline": "Celerity" }, { "discipline": "Obfuscate" }],
+            "archetypeList": [
+                { "archetype": "" }
             ],
-            ruleSet: "Core"
+            "ruleSet": "ChildrenOfCaine"
         },
         {
-            clanName: "Gangrel",
-            clanDisciplines: [{ discpline: "Animalism" }, { discipline: "Fortitude" }, { discipline: "Protean" }],
-            archetypeList: [{archetype: "Uncaged Jailbird"}],
-            ruleSet: "Core"
+            "clanName": "Brujah",
+            "clanDisciplines": [{ "discipline": "Celerity" }, { "discipline": "Potence" }, { "discipline": "Presence" }],
+            "archetypeList": [
+                { "archetype": "Cancer In The System" },
+                { "archetype": "Voice Of The People" },
+                { "archetype": "Blood Worshipper" },
+                { "archetype": "Trolling Punk" },
+                { "archetype": "Monster In Disguise" }
+            ],
+            "ruleSet": "Core"
+        },
+        {
+            "clanName": "Caitiff",
+            "clanDisciplines": [{ "discipline": "" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
+        },
+        {
+            "clanName": "Gangrel",
+            "clanDisciplines": [{ "discipline": "Animalism" }, { "discipline": "Fortitude" }, { "discipline": "Protean" }],
+            "archetypeList": [{"archetype": "Uncaged Jailbird"}],
+            "ruleSet": "Core"
         },
         { 
-            clanName: "Hecata" 
+            "clanName": "Hecata",
+            "clanDisciplines": [{ "discipline": "Auspex" }, { "discipline": "Fortitude" }, { "discipline": "Oblivion" }],
+            "archetypeList": [
+                { "archetype": "" }
+            ],
+            "ruleSet": "ChildrenOfCaine"
         },
         { 
-            clanName: "Lasombra" 
+            "clanName": "Lasombra",
+            "clanDisciplines": [{ "discipline": "Dominate" }, { "discipline": "Oblivion" }, { "discipline": "Potence" }],
+            "archetypeList": [
+                { "archetype": "" }
+            ],
+            "ruleSet": "ChildrenOfCaine" 
         },
         { 
-            clanName: "Malkavian" 
+            "clanName": "Malkavian",
+            "clanDisciplines": [{ "discipline": "Auspex" }, { "discipline": "Dominate" }, { "discipline": "Obfuscate" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
         },
         { 
-            clanName: "Ministry" 
+            "clanName": "Ministry",
+            "clanDisciplines": [{ "discipline": "Obfuscate" }, { "discipline": "Presence" }, { "discipline": "Protean" }],
+            "archetypeList": [
+                { "archetype": "" }
+            ],
+            "ruleSet": "ChildrenOfCaine" 
         },
         { 
-            clanName: "Nosferatu" 
+            "clanName": "Nosferatu",
+            "clanDisciplines": [{ "discipline": "Animalism" }, { "discipline": "Obfuscate" }, { "discipline": "Potence" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
         },
         { 
-            clanName: "Ravnos" 
+            "clanName": "Ravnos",
+            "clanDisciplines": [{ "discipline": "Animalism" }, { "discipline": "Obfuscate" }, { "discipline": "Presence" }],
+            "archetypeList": [
+                { "archetype": "" }
+            ],
+            "ruleSet": "ChildrenOfCaine"
         },
         { 
-            clanName: "Salubri" 
+            "clanName": "Salubri",
+            "clanDisciplines": [{ "discipline": "Auspex" }, { "discipline": "Dominate" }, { "discipline": "Fortitude" }],
+            "archetypeList": [
+                { "archetype": "" }
+            ],
+            "ruleSet": "ChildrenOfCaine" 
         },
         { 
-            clanName: "Toreador" 
+            "clanName": "Toreador",
+            "clanDisciplines": [{ "discipline": "Auspex" }, { "discipline": "Celerity" }, { "discipline": "Presence" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
+        },
+        {
+            "clanName": "Thin Bloods",
+            "clanDisciplines": [{ "discipline": "" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
         },
         { 
-            clanName: "Tremere" 
+            "clanName": "Tremere",
+            "clanDisciplines": [{ "discipline": "Auspex" }, { "discipline": "Dominate" }, { "discipline": "Blood Sorcery" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
         },
         { 
-            clanName: "Tzimisce" 
+            "clanName": "Tzimisce",
+            "clanDisciplines": [{ "discipline": "Animalism" }, { "discipline": "Dominate" }, { "discipline": "Protean" }],
+            "archetypeList": [
+                { "archetype": "" }
+            ],
+            "ruleSet": "ChildrenOfCaine" 
         },
         { 
-            clanName: "Ventrue" 
+            "clanName": "Ventrue",
+            "clanDisciplines": [{ "discipline": "Dominate" }, { "discipline": "Fortitude" }, { "discipline": "Presence" }],
+            "archetypeList": [{"archetype": ""}],
+            "ruleSet": "Core"
         }
     ]
 };
