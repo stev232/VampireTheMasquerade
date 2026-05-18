ced4128ad42fa81de1d7778e671422d9577c125f
Stev232 <stephen.merki232@gmail.com>
2026-05-17 20:02:12 -0500

    Added Disciplines to the DisciplineList file.

--- assets/js/disciplineList.js
+++ assets/js/disciplineList.js
@@ -31,7 +31,28 @@ const disciplineList = {
             {"name": "Telepathy", "level": 5, "ruleSet": "Core"},
             {"name": "Unburdening The Bestial SOul", "level": 5, "ruleSet": "ChildrenOfCaine"}
         ] }, 
-        { "discipline": "Blood Sorcery" }, 
+        { "discipline": "Blood Sorcery", "abilities": [
+            {"name": "Corrosive Vitae", "level": 1, "ruleSet": "Core"},
+            {"name": "A Taste For Blood", "level": 1, "ruleSet": "Core"},
+            {"name": "Extinguish Vitae", "level": 2, "ruleSet": "Core"},
+            {"name": "Scour Secrets", "level": 2, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Blood Of Potency", "level": 3, "ruleSet": "Core"},
+            {"name": "Scorpion's Touch", "level": 3, "ruleSet": "Core"},
+            {"name": "Blood Aegis", "level": 4, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Theft Of Vitae", "level": 4, "ruleSet": "Core"},
+            {"name": "Baal's Caress", "level": 5, "ruleSet": "Core"},
+            {"name": "Cauldron Of Blood", "level": 5, "ruleSet": "Core"}
+        ], "rituals": [
+            {"name": "Blood Walk", "level": 1, "ruleSet": "Core"},
+            {"name": "Clining Of The Insect", "level": 1, "ruleSet": "Core"},
+            {"name": "Craft Bloodstone", "level": 1, "ruleSet": "Core"},
+            {"name": "Wake With Evening's Freshness", "level": 1, "ruleSet": "Core"},
+            {"name": "Ward Against Ghouls", "level": 1, "ruleSet": "Core"},
+            {"name": "Communicate With Kindred Sire", "level": 2, "ruleSet": "Core"},
+            {"name": "Eyes Of Babel", "level": 2, "ruleSet": "Core"},
+            {"name": "Illuminate The Trail Of Prey", "level": 2, "ruleSet": "Core"},
+            {"name": "Truth Of Blood", "level": 2, "ruleSet": "Core"}
+        ] }, 
         { "discipline": "Celerity", "abilities": [
             {"name": "Cat's Grace", "level": 1, "ruleSet": "Core"},
             {"name": "Rapid Reflexes", "level": 1, "ruleSet": "Core"},
@@ -112,11 +133,60 @@ const disciplineList = {
             {"name": "Skuld Fulfilled", "level": 5, "ruleSet": "ChildrenOfCaine"},
             {"name": "Tenebrous Avatar", "level": 5, "ruleSet": "ChildrenOfCaine"}
         ], "ceremonies": [
-            {"name": "The Gift Of False Life", "level": 1, "ruleSet": "ChildrenOfCaine"}
+            {"name": "The Gift Of False Life", "level": 1, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Summon Spirit", "level": 1, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Awaken The Homuncular Servant", "level": 2, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Compel Spirit", "level": 2, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Host Spirit", "level": 3, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Shambling Hordes", "level": 3, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Bind The Spirit", "level": 4, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Split The Veil", "level": 4, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Lazarene Blessing", "level": 5, "ruleSet": "ChildrenOfCaine"}
     ] }, 
-        { "discipline": "Potence" }, 
-        { "discipline": "Presence" },
-        { "discipline": "Protean" }, 
+        { "discipline": "Potence", "abilities": [
+            {"name": "Lethal Body", "level": 1, "ruleSet": "Core"},
+            {"name": "Soaring Leap", "level": 1, "ruleSet": "Core"},
+            {"name": "Prowess", "level": 2, "ruleSet": "Core"},
+            {"name": "Relentless Grasp", "level": 2, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Brutal Feed", "level": 3, "ruleSet": "Core"},
+            {"name": "Spark Of Rage", "level": 3, "ruleSet": "Core"},
+            {"name": "Wrecker", "level": 3, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Uncanny Grip", "level": 3, "ruleSet": "Core"},
+            {"name": "Crash Down", "level": 4, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Draught Of Hight", "level": 4, "ruleSet": "Core"},
+            {"name": "EarthShock", "level": 5, "ruleSet": "Core"},
+            {"name": "Fist Of Caine", "level": 5, "ruleSet": "Core"},
+            {"name": "Subtle Hammer", "level": 5, "ruleSet": "ChildrenOfCaine"}
+        ] }, 
+        { "discipline": "Presence", "abilities": [
+            {"name": "Awe", "level": 1, "ruleSet": "Core"},
+            {"name": "Daunt", "level": 1, "ruleSet": "Core"},
+            {"name": "Eyes Of The Serpent", "level": 1, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Lingering Kiss", "level": 2, "ruleSet": "Core"},
+            {"name": "Melpominee", "level": 2, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Dread Gaze", "level": 3, "ruleSet": "Core"},
+            {"name": "Entrancement", "level": 3, "ruleSet": "Core"},
+            {"name": "Thrown Voice", "level": 3, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Irresistible Voice", "level": 4, "ruleSet": "Core"},
+            {"name": "Suffuse The Edifice", "level": 4, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Summon", "level": 4, "ruleSet": "Core"},
+            {"name": "Majesty", "level": 5, "ruleSet": "Core"},
+            {"name": "Star Magnetism", "level": 5, "ruleSet": "Core"}
+        ] },
+        { "discipline": "Protean", "abilities": [
+            {"name": "Eyes Of The Beast", "level": 1, "ruleSet": "Core"},
+            {"name": "Weight Of The Feather", "level": 1, "ruleSet": "Core"},
+            {"name": "Feral Weapons", "level": 2, "ruleSet": "Core"},
+            {"name": "Vicissitude", "level": 2, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Earth Meld", "level": 3, "ruleSet": "Core"},
+            {"name": "Fleshcrafting", "level": 3, "ruleSet": "ChildrenOfCaine"},
+            {"name": "ShapeChange", "level": 3, "ruleSet": "Core"},
+            {"name": "Horrid Form", "level": 4, "ruleSet": "ChildrenOfCaine"},
+            {"name": "Metamorphosis", "level": 4, "ruleSet": "Core"},
+            {"name": "Mist Form", "level": 5, "ruleSet": "Core"},
+            {"name": "One With The Land", "level": 5, "ruleSet": "ChildrenOfCaine"},
+            {"name": "The Unfettered Heart", "level": 5, "ruleSet": "Core"}
+        ] }, 
         { "discipline": "Thin-Blood Alchemy" }
     ]
 };
