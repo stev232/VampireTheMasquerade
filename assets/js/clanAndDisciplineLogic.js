6a779c4cd66ffc37b2d7ad8b08593412bd849a82
Stephen Merki <stephen.merki232@gmail.com>
2026-05-21 13:26:12 -0500

    Made the ability lists self clear in the event that a new discipline is selected.

--- assets/js/clanAndDisciplineLogic.js
+++ assets/js/clanAndDisciplineLogic.js
@@ -1,3 +1,5 @@
+const disciplineDropDowns = ["disciplineOne", "disciplineTwo", "disciplineThree"];
+
 function setAvailableDisciplines(clan) {
   $('.disciplineOptions').remove();
   let found = false;
@@ -7,26 +9,26 @@ function setAvailableDisciplines(clan) {
       if (clan == clanList.fifthEdition[counter].clanName) {
         // console.log(counter);
         $('#disciplineDropDownOne').append(`<option class="disciplineOptions" selected value=""> -- select an option -- </option>`);
-        $('#disciplineDropDownTwo').append(`<option class="disciplineOptions" selected value> -- select an option -- </option>`);
-        $('#disciplineDropDownThree').append(`<option class="disciplineOptions" selected value> -- select an option -- </option>`);
+        $('#disciplineDropDownTwo').append(`<option class="disciplineOptions" selected value=""> -- select an option -- </option>`);
+        $('#disciplineDropDownThree').append(`<option class="disciplineOptions" selected value=""> -- select an option -- </option>`);
         if (clanList.fifthEdition[counter].clanDisciplines[0].discipline != 'All') {
           for (let i = 0; i < clanList.fifthEdition[counter].clanDisciplines.length; i++) {
             $('#disciplineDropDownOne').append(
-              `<option class="disciplineOptions" value="${clanList.fifthEdition[counter].clanDisciplines[i]}">
+              `<option class="disciplineOptions" value="${clanList.fifthEdition[counter].clanDisciplines[i].discipline}">
                 ${clanList.fifthEdition[counter].clanDisciplines[i].discipline}
               </option>`
             )
           }
           for (let i = 0; i < clanList.fifthEdition[counter].clanDisciplines.length; i++) {
             $('#disciplineDropDownTwo').append(
-              `<option class="disciplineOptions" value="${clanList.fifthEdition[counter].clanDisciplines[i]}">
+              `<option class="disciplineOptions" value="${clanList.fifthEdition[counter].clanDisciplines[i].discipline}">
                 ${clanList.fifthEdition[counter].clanDisciplines[i].discipline}
               </option>`
             )
           }
           for (let i = 0; i < clanList.fifthEdition[counter].clanDisciplines.length; i++) {
             $('#disciplineDropDownThree').append(
-              `<option class="disciplineOptions" value="${clanList.fifthEdition[counter].clanDisciplines[i]}">
+              `<option class="disciplineOptions" value="${clanList.fifthEdition[counter].clanDisciplines[i].discipline}">
                 ${clanList.fifthEdition[counter].clanDisciplines[i].discipline}
               </option>`
             )
@@ -73,8 +75,24 @@ function setAvailableDisciplines(clan) {
   }
 }
 
-function setAvailableAbilities(discipline) {
-
+function setAvailableAbilities(discipline, columnNum) {
+  if (discipline) {
+    $(`.${disciplineDropDowns[columnNum]}`).append(
+      `<option disabled selected class="abilityOptions" value="">---------------</option>`
+    );
+    for (let x = 0; x < disciplineList.disciplines.length; x++) {
+      if (disciplineList.disciplines[x].discipline == discipline) {
+        for (let y = 0; y < disciplineList.disciplines[x].abilities.length; y++) {
+          $(`.${disciplineDropDowns[columnNum]}`).append(
+            `<option class="abilityOptions" value="${disciplineList.disciplines[x].abilities[y].name}">
+              ${disciplineList.disciplines[x].abilities[y].name} | Level: ${disciplineList.disciplines[x].abilities[y].level}
+            </option>`
+          )
+        }
+      }
+    }
+    console.log(discipline);
+  }
 }
 
 function populateClanList() {
@@ -90,4 +108,25 @@ $('#clan').on("click", function () {
   //Stops all other listeners from being called. This was implimented because of actions occurring twice instead of only running once.
   event.stopImmediatePropagation();
   setAvailableDisciplines($('#clan')[0].value);
+})
+
+$('#disciplineDropDownOne').on("click", function () {
+  //Stops all other listeners from being called. This was implimented because of actions occurring twice instead of only running once.
+  event.stopImmediatePropagation();
+  $(`.${disciplineDropDowns[0]}`).children('.abilityOptions').remove();
+  setAvailableAbilities($('#disciplineDropDownOne')[0].value, 0);
+})
+
+$('#disciplineDropDownTwo').on("click", function () {
+  //Stops all other listeners from being called. This was implimented because of actions occurring twice instead of only running once.
+  event.stopImmediatePropagation();
+  $(`.${disciplineDropDowns[1]}`).children('.abilityOptions').remove();
+  setAvailableAbilities($('#disciplineDropDownTwo')[0].value, 1);
+})
+
+$('#disciplineDropDownThree').on("click", function () {
+  //Stops all other listeners from being called. This was implimented because of actions occurring twice instead of only running once.
+  event.stopImmediatePropagation();
+  $(`.${disciplineDropDowns[2]}`).children('.abilityOptions').remove();
+  setAvailableAbilities($('#disciplineDropDownThree')[0].value, 2);
 })
