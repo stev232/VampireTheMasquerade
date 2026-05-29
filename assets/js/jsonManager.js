4c13c0dfe2263ea69baf29ed12925233448ed8f8
Stephen Merki <stephen.merki232@gmail.com>
2026-05-29 14:54:03 -0500

    Added download button along with a toast to notify the user of required fields.

--- assets/js/jsonManager.js
+++ assets/js/jsonManager.js
@@ -1,7 +1,58 @@
-function downloadJSON(characterName) {
-  const fileName = `${characterName}.json`;
+const jsFrame = new JSFrame();
+    
+function jsonPackager(data) {
+  console.log("json Packager");
+  console.log($('#characterName')[0].value);
+  if (!$('#characterName')[0].value) {
+    jsFrame.showToast({
+      style: {
+        border: '2px solid red',
+        borderRadius: '2px',
+        backgroundColor: '#000000',
+      },
+      html: '<span stlye="color: #DD0000; font-weight: bold;">&#42;</span>&nbsp;<span style="color: white;">Please add a character name!</span>', align: 'top', duration: 4000
+    });
+  } else {
+    jsFrame.showToast({
+      style: {
+        border: '2px solid red',
+        borderRadius: '2px',
+        backgroundColor: '#000000',
+      },
+      html: '<span style="color: white;">WIP!!! This feature is still in development!</span>', align: 'top', duration: 4000
+    });
+  }
+  return ("some text");
 }
 
-function uploadJSON() {
-
+function downloadJSON(data) {
+  // const fileName = `${data.characterName}.json`;
+  // jsonPackager(data);
+  console.log(data);
 }
+
+function uploadJSON(data) {
+  console.log(data);
+}
+
+// function main(event) {
+//   switch (event.command) {
+//     case "download":
+//       downloadJSON(event)
+//       break;
+//     case "upload":
+//       uploadJSON(event);
+//       break;
+
+//     default:
+//       console.log(event);
+//       break;
+//   }
+// }
+
+// main(event);
+
+$('#downloadButton').on('click', function (event) {
+  event.stopImmediatePropagation();
+  downloadJSON(jsonPackager($('.characterSheetBody')));
+});
